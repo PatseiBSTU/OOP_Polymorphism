d2279dade598b5253420b52340305e2afa2b31a6
PatseiBSTU <n.patsei@belstu.by>
2018-11-03 23:41:05 +0300

    Add files via upload

---
+++ Polymorphism/WithoutPolymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutPolymorphism
+{
+    
+        public class СombatUnit
+        {
+            public int Energy { get; set; } = 100;
+          public void Battle() => Console.WriteLine("-----СombatUnit------");
+            
+        }
+        public class Elf : СombatUnit
+        {
+            public void Battle() => Console.WriteLine("++++++++Elf++++++");
+        }
+
+        public class Goblin : СombatUnit
+        {
+            public void Battle() => Console.WriteLine("~~~~~~~Goblin~~~~~~");
+        }
+    class Program
+       {  private static void Main(string[] args)
+         {
+            Elf anna = new Elf();
+            Elf olgA= new Elf();
+            Goblin oleg = new Goblin();
+            Elf nikita = new Elf();
+            Goblin polina = new Goblin();
+
+            СombatUnit[] army = { anna, oleg, olgA, nikita, polina };
+            foreach (var unit in army)
+            {
+                unit.Battle();
+            }
+
+                                         //  вызов методов по типу ссылки
+        }
+       
+    }
+}
---
+++ Polymorphism/WithoutPolymorphism/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WithoutPolymorphism")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WithoutPolymorphism")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("59282709-490f-40b9-bb84-d0a6d1b8c1ed")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
